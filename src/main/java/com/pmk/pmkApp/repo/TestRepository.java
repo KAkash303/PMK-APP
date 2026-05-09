94f4c2e0505cc413d1cb481485b2464295722128
Sai Surender <saisivas200@gmail.com>
2026-05-09 13:12:09 +0530

    Cmmit

--- src/main/java/com/pmk/pmkApp/repo/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pmk.pmkApp.repo;
-
-
-import com.pmk.pmkApp.model.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
